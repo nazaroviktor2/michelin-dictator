6eb487a4f2a93c5ce03d189f1b355d81a06d74d6
kirillsev1 <kirillprih47@gmail.com>
2022-11-29 23:18:50 +0300

    speed

--- python/reading_speed.py
+++ python/reading_speed.py
@@ -8,6 +8,6 @@ def speed(text, average_error, average_speed):
     arr = [i for i in '!@#$%^&()_-,./;:|\~`']
     number_of_words = len(replace_all(text, arr).split())
     result_speed = number_of_words//average_speed
-    return [result_speed - error, result_speed + error]
+    return [result_speed - average_error, result_speed + average_error]
 
-print(speed('GYTFdhvegv ,kmlew fwe#l ,'))
+# print(speed('GYTFdhvegv ,kmlew fwe#l ,'))
