efb804e5f748e233d76df60e2d8ca9354125a597
nazaroviktor2 <nazarov20012004@gmail.com>
2022-12-03 21:58:48 +0300

    debug

--- michelinDictator/michelinDictator/settings.py
+++ michelinDictator/michelinDictator/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0ov#*c1vckbq5rk5!!m@+ll!jc5bhk4ja(^2o44+9^nj^0j0jo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
