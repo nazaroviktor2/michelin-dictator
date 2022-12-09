0acd62013dc5f7ab68be1e6056b3be570edd00fe
nazaroviktor2 <nazarov20012004@gmail.com>
2022-12-09 15:28:08 +0300

    add allow video and auto

--- michelinDictator/users/models.py
+++ michelinDictator/users/models.py
@@ -9,5 +9,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     is_editor = models.BooleanField(default=False, null=False)
-
-
+    allow_video = models.BooleanField(default=True, null=False)
+    allow_auto_transition = models.BooleanField(default=True, null=False)
