ce44c09aa012d5cbe0a1761bddcc5c032c4e9e7a
nazaroviktor2 <nazarov20012004@gmail.com>
2022-12-12 10:19:45 +0300

    save profile

--- michelinDictator/users/views.py
+++ michelinDictator/users/views.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 from django.contrib.auth import logout
 from django.contrib.auth.forms import AuthenticationForm
@@ -32,20 +33,32 @@ def logout_user(request):
 
 
 def user_profile(request):
-    user_audio = Audio.objects.filter(user=request.user)
-    count_audio = len(user_audio)
-    cards_id = [card.id for card in Card.objects.filter(user=request.user)]
-    count_report = 0
-    audios_time = [audio.duration for audio in user_audio]
-    voicing_time = datetime.timedelta()
-    for time in audios_time:
-        voicing_time += time
-    voicing_time -= datetime.timedelta(microseconds=voicing_time.microseconds)
-    for card_id in cards_id:
-        count_report += len(Report.objects.filter(card=card_id))
-    return render(request, "profile.html", {"count_audio": count_audio, "count_card": len(cards_id),
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            name = request.headers.get("Name")
+            if name == "Save":
+                video =  (json.loads(request.body)).get("video")
+                auto_transition =  (json.loads(request.body)).get("auto_transition")
+                request.user.allow_auto_transition = auto_transition
+                request.user.allow_video = video
+                print(request.user.id)
+                request.user.save()
+        user_audio = Audio.objects.filter(user=request.user)
+        count_audio = len(user_audio)
+        cards_id = [card.id for card in Card.objects.filter(user=request.user)]
+        count_report = 0
+        audios_time = [audio.duration for audio in user_audio]
+        voicing_time = datetime.timedelta()
+        for time in audios_time:
+            voicing_time += time
+        voicing_time -= datetime.timedelta(microseconds=voicing_time.microseconds)
+        for card_id in cards_id:
+            count_report += len(Report.objects.filter(card=card_id))
+        return render(request, "profile.html", {"count_audio": count_audio, "count_card": len(cards_id),
                                             "count_report": count_report,
                                             "voicing_time":voicing_time})
+    else:
+        return render(request, "profile.html")
 
 
 def successful(request):
